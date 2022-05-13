ebc7286622645e4c25d4f9bd8a98a7b9a9c3a57c
Duplesis <59741317+Aratolle@users.noreply.github.com>
2022-05-13 13:21:55 +0100

    Modified a few comments

--- src/main/java/com/sparta/sort/MergeSorter.java
+++ src/main/java/com/sparta/sort/MergeSorter.java
@@ -1,15 +1,14 @@
 package com.sparta.sort;
 
-import java.util.Arrays;
-
 public class MergeSorter {
 
     public static int[] sortArray(int[] inputArray) {
+        // As we want to sort the entire array the end point is the same as the length
         mergeSortArray(inputArray, (inputArray.length));
         return inputArray;
     }
 
-    // A recursive method that sorts an input array using "Merge-Sort"
+    // A recursive function that sorts an input array using the "Merge-Sort" method
     private static void mergeSortArray(int[] arrayToSort, int endPoint) {
         // If the array is of length 1 or lower, job is already done!
         if (arrayToSort.length <= 1) {
@@ -20,7 +19,6 @@ public class MergeSorter {
         int midPoint = endPoint / 2;
         int[] left = new int[midPoint];
         int[] right = new int[endPoint - midPoint];
-
 
         // Populate left and right arrays by splitting the original array into 'left' and 'right' values
         System.arraycopy(arrayToSort, 0, left, 0, midPoint);
