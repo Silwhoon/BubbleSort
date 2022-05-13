f6f398fdb8312a19df5e02d3c5c3710a9ef02338
Duplesis <59741317+Aratolle@users.noreply.github.com>
2022-05-13 11:10:16 +0100

    Implemented Merge Sort method

--- src/main/java/com/sparta/sort/MergeSorter.java
+++ src/main/java/com/sparta/sort/MergeSorter.java
@@ -5,24 +5,55 @@ import java.util.Arrays;
 public class MergeSorter {
 
     public static int[] sortArray(int[] inputArray) {
-        mergeSortArray(inputArray, 0, inputArray.length);
+        mergeSortArray(inputArray, (inputArray.length));
         return inputArray;
     }
 
-    private static void mergeSortArray(int[] arrayToSort, int startPoint, int endPoint) {
-        int midPoint = endPoint / 2;
-
-        // Check if the specified start/end point has a size greater than 1, if it does then split it in half
-        // and attempt to 'merge sort' the new parts
-        if (startPoint < endPoint) {
-            // Purposefully call this method recursively, with the 2 new halves of the passed array
-            mergeSortArray(arrayToSort, startPoint, midPoint);
-            mergeSortArray(arrayToSort, midPoint + 1, endPoint);
+    // A recursive method that sorts an input array using "Merge-Sort"
+    private static void mergeSortArray(int[] arrayToSort, int endPoint) {
+        // If the array is of length 1 or lower, job is already done!
+        if (arrayToSort.length <= 1) {
+            return;
         }
 
-        //System.arraycopy(arrayTwo, arrayTwoPosition, ret, i, arrayTwo.length - arrayTwoPosition);
-
-        int[] left = new int[midPoint - startPoint];
+        // Instantiate a left and right array using the midpoint to determine size
+        int midPoint = endPoint / 2;
+        int[] left = new int[midPoint];
         int[] right = new int[endPoint - midPoint];
+
+
+        // Populate left and right arrays by splitting the original ray into 'left' and 'right' values
+        System.arraycopy(arrayToSort, 0, left, 0, midPoint);
+
+        if (endPoint - midPoint >= 0) {
+            System.arraycopy(arrayToSort, midPoint, right, 0, endPoint - midPoint);
+        }
+
+        // Purposefully call this method recursively, with the 2 new halves of the passed array
+        mergeSortArray(left, midPoint);
+        mergeSortArray(right, endPoint - midPoint);
+
+        // Merge the split arrays back into the original static array
+        int leftCounter = 0;
+        int rightCounter = 0;
+        int retCounter = 0;
+
+        // While we still have values in BOTH left and right, compare them and sort
+        while (leftCounter < midPoint && rightCounter < (endPoint - midPoint)) {
+            if (left[leftCounter] <= right[rightCounter]) {
+                arrayToSort[retCounter++] = left[leftCounter++];
+            } else {
+                arrayToSort[retCounter++] = right[rightCounter++];
+            }
+        }
+
+        // After handling all the values either in the left or right array, add the remainder of the last array to the
+        // original static array
+        while (leftCounter < midPoint) {
+            arrayToSort[retCounter++] = left[leftCounter++];
+        }
+        while (rightCounter < (endPoint - midPoint)) {
+            arrayToSort[retCounter++] = right[rightCounter++];
+        }
     }
 }
--- src/test/java/com/sparta/sort/MergeSorterTest.java
+++ src/test/java/com/sparta/sort/MergeSorterTest.java
@@ -6,9 +6,72 @@ import org.junit.jupiter.api.Test;
 public class MergeSorterTest {
 
     @Test
-    public void mergeAndSortArraySimple() {
+    public void mergeAndSortArraySimpleEvenAmountOfIndexes() {
         int[] exampleArray = {0, 3, 2, 5, 4, 1};
         int[] expectedResult = {0, 1, 2, 3, 4, 5};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArraySimpleOddAmountOfIfIndexes() {
+        int[] exampleArray = {0, 3, 2, 5, 4};
+        int[] expectedResult = {0, 2, 3, 4, 5};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArrayEmpty() {
+        int[] exampleArray = {};
+        int[] expectedResult = {};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArraySingleIndex() {
+        int[] exampleArray = {5};
+        int[] expectedResult = {5};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArrayDoubleIndex() {
+        int[] exampleArray = {5, -1};
+        int[] expectedResult = {-1, 5};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArrayNegative() {
+        int[] exampleArray = {0, 3, 2, -1, -2, 1};
+        int[] expectedResult = {-2, -1, 0, 1, 2, 3};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArrayFewIdenticalIndexes() {
+        int[] exampleArray = {0, 3, 2, 2, 1, 1};
+        int[] expectedResult = {0, 1, 1, 2, 2, 3};
+        int[] actualResult = MergeSorter.sortArray(exampleArray);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void mergeAndSortArrayIdenticalIndexes() {
+        int[] exampleArray = {1, 1, 1};
+        int[] expectedResult = {1, 1, 1};
         int[] actualResult = MergeSorter.sortArray(exampleArray);
 
         Assertions.assertArrayEquals(expectedResult, actualResult);
