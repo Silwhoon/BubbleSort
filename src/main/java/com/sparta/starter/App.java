a37b47f7e985304567796b50727f269cd70bd23b
Silwhoon <59741317+Aratolle@users.noreply.github.com>
2022-05-18 16:17:58 +0100

    Added log4j

--- src/main/java/com/sparta/starter/App.java
+++ src/main/java/com/sparta/starter/App.java
@@ -1,6 +1,11 @@
 package com.sparta.starter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+    private static final Logger logger = LogManager.getLogger("sortManager");
+
     public static void main(String[] args) {
         SorterLoader.start();
     }
